71780a341182e1d5a1998f00e0709a92c61ced87
Valentin Delaye <jonesbusy@users.noreply.github.com>
2025-10-08 09:44:33 +0200

    Plugin init

---
+++ README.md
@@ -0,0 +1,20 @@
+# Jackson 3 API Plugin
+
+[![Jenkins Plugin](https://img.shields.io/jenkins/plugin/v/jackson3-api.svg)](https://plugins.jenkins.io/jackson3-api)
+[![GitHub release](https://img.shields.io/github/release/jenkinsci/jackson3-api-plugin.svg?label=changelog)](https://github.com/jenkinsci/jackson3-api-plugin/releases/latest)
+[![Jenkins Plugin Installs](https://img.shields.io/jenkins/plugin/i/jackson3-api.svg?color=blue)](https://plugins.jenkins.io/jackson3-api)
+
+This plugin exposes the [Jackson 3](https://github.com/FasterXML/jackson) API to Jenkins plugins.
+
+## Changelog
+
+* See [GitHub Releases](https://github.com/jenkinsci/jackson3-api-plugin/releases) for recent versions
+
+## Developer documentation
+
+See the [Jenkins Developer Documentation](https://jenkins.io/doc/developer/book/).
+
+Cheatsheet:
+
+* To build the plugin locally: `mvn clean verify`
+* To test in a local Jenkins instance: `mvn hpi:run`
---
+++ src/test/java/tools/jackson/databind/json/JSONMapperTest.java
@@ -0,0 +1,62 @@
+package tools.jackson.databind.json;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.nio.charset.StandardCharsets;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.jvnet.hudson.test.junit.jupiter.RealJenkinsExtension;
+
+/**
+ * Basic tests for {@link JsonMapper}.
+ */
+class JSONMapperTest {
+
+    @RegisterExtension
+    private final RealJenkinsExtension extension = new RealJenkinsExtension();
+
+    @Test
+    void smokes() throws Throwable {
+        extension.then(r -> {
+            JsonMapper mapper = new JsonMapper();
+            // language=json
+            String content =
+                    """
+                    {
+                      "bar": {
+                          "id": "123"
+                    }
+                    }""";
+            Foo foo = mapper.readValue(content.getBytes(StandardCharsets.UTF_8), Foo.class);
+            assertNotNull(foo.getBar());
+            assertEquals("123", foo.getBar().getId());
+        });
+    }
+
+    @SuppressWarnings("unused")
+    static class Foo {
+        private Bar bar;
+
+        public Bar getBar() {
+            return bar;
+        }
+
+        public void setBar(Bar bar) {
+            this.bar = bar;
+        }
+    }
+
+    @SuppressWarnings("unused")
+    static class Bar {
+        private String id;
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+    }
+}
